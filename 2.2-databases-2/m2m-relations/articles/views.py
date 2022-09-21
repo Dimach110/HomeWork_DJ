3048014ead1dd0b361e1926a679639e09525f9ea
dimach110 <dima110@mail.ru>
2022-09-21 11:38:33 +0300

    Выполнил все условия задания

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-import pprint
 from articles.models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    news = Article.objects.all()
+    news = Article.objects.prefetch_related('tag').all()
     context = {
         'object_list': news,
 
